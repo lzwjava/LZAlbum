d598c71fee98e9e3244360f5bcb82310cf722616
lzwjava <lzwjava@gmail.com>
2016-02-28 22:44:56 +0800

    增加上传到 fir.im 的脚本

--- package.sh
+++ package.sh
@@ -1,2 +1,5 @@
+rm -rf ./build/LZAlbum.ipa && \
 pod install --verbose --no-repo-update &&  \
-xcodebuild -workspace LZAlbum.xcworkspace -scheme LZAlbum archive
+xcodebuild -workspace LZAlbum.xcworkspace -scheme LZAlbum archive -archivePath ./build/LZAlbum.xcarchive && \
+xcodebuild -exportArchive -exportFormat ipa -archivePath build/LZAlbum.xcarchive -exportPath build/LZAlbum.ipa && \
+fir p build/LZAlbum.ipa -T eeca39297cfda6e31a67c9b66e1edd07
